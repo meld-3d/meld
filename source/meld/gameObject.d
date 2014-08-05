95a7708ac64430efae99b5a0383b50bff5e98b88
Alex Parker <3zanders@gmail.com>
2014-08-05 23:14:41 +0100

    started game objects

---
+++ source/meld/gameObject.d
@@ -0,0 +1,64 @@
+import meld;
+
+abstract class Component(T)
+{
+package:
+	static T[int] componentMap;
+	static T[] componentList;
+public:
+	immutable GameObject gameObject;
+}
+
+class GameObject
+{
+private:
+	immutable int instanceID;
+
+public:
+	T Add(T)(...)
+	{
+		Component!T component = null;//new T(...);
+		T.componentList ~= component;
+		T.componentMap[instanceID] = component;
+		return component;
+	}
+
+	static T[] GetComponentList(T)()
+	{
+		return T.componentList;
+	}
+}
+
+void Test()
+{
+	GameObject o;
+	Mesh mesh;
+	o.Add!MeshRenderer(mesh);
+
+	MeshRenderer.Draw();
+}
+
+class MeshRenderer : Component!MeshRenderer
+{
+private:
+	Mesh mesh;
+
+public:
+	this(Mesh mesh)
+	{
+		this.mesh = mesh;
+	}
+
+	static void Draw()
+	{
+		foreach (MeshRenderer renderer; GameObject.GetComponentList!MeshRenderer())
+		{
+			renderer.DrawInternal();
+		}
+	}
+
+	void DrawInternal()
+	{
+
+	}
+}
